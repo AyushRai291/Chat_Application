d2f1da950a328df80e83dd3d3e4d19da9f7fa272
Ayush Rai <ayushrai9644@gmail.com>
2026-06-27 17:08:04 +0530

    Enable local network testing

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
-import cors from "cors"; 
-//cross origin resouce sharing allow krta hai frontend port ko data access
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { uploadsDir } from "./middlewares/upload.middleware.js";
 import testRoutes from "./routes/test.routes.js";
@@ -11,25 +10,22 @@ import conversationRoutes from "./routes/conversation.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
 
-
 const app = express();
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true //cookies allowed
-}));
 app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use("/uploads", express.static(uploadsDir));
 
-
-
 app.get("/", (req, res) => {
   res.send("API running");
 });
-
-
 
 app.use("/api/test", testRoutes);
 app.use("/api/auth", authRoutes);
@@ -38,8 +34,7 @@ app.use("/api/conversations", conversationRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-
-
 app.use(notFound);
 app.use(errorHandler);
+
 export default app;
